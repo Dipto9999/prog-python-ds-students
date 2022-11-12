ac1295daa774e51e30e35786e2d24b3f4afd34e3
Muntakim Rahman <muntakim.rahman@gmail.com>
2022-11-12 14:09:24 -0500

    Assignment 7 and 8 Not Fully Complete

--- release/assignment7/sampling.py
+++ release/assignment7/sampling.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 
-def sample_dataframe(data, grouping_col, N = 1):
+def sample_dataframe(data, grouping_col, N=1):
     """
     Given a dataframe, return a smaller sample of the dataframe
     sampling N rows from each specified group
-    
+
     Parameters
     ----------
     data : pandas.core.frame.DataFrame
@@ -15,12 +15,12 @@ def sample_dataframe(data, grouping_col, N = 1):
     N : int, optional
         The number of rows to sample from each group (The default value is 1
         which implies a single observation)
-        
+
     Returns
     -------
     pandas.core.frame.DataFrame
         The new sampled dataframe
-        
+
     Examples
     --------
     >>> sample_dataframe(pokemon, 'legendary'])
@@ -28,14 +28,13 @@ def sample_dataframe(data, grouping_col, N = 1):
     411 Burmy     412     29        45      bug     4      0
     640 Tornadus  641     100       80      flying  5      1
     """
-    
-    df_grouped = data.groupby(grouping_col)
-    
-    sampled_df = None
-    
-    for group, rows in df_grouped: 
-        group_sampling =  df_grouped.get_group(group).sample(N)
-        sampled_df = pd.concat([sampled_df, group_sampling])
-    
-    return sampled_df
 
+    df_grouped = data.groupby(grouping_col)
+
+    sampled_df = None
+
+    for group, rows in df_grouped:
+        group_sampling = df_grouped.get_group(group).sample(N)
+        sampled_df = pd.concat([sampled_df, group_sampling])
+
+    return sampled_df
